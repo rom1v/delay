f909de5b624d6cfdaba9cdd08175a95dfb807365
Romain Vimont (®om) <rom@rom1v.com>
2014-01-12 16:19:40 +0100

    Commandline arguments parser
    Parse commandline arguments:
      delay [-b <dtbufsize>] <delay>

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "dtbuf.h"
+#include "errno.h"
 #include "time_ms.h"
 
 /*
@@ -16,7 +18,88 @@ struct pollfd fds_all[] = {
   {1 /* stdout */ , POLLOUT | POLLHUP}
 };
 
-#define DELAY 1000              // 1 second
+// default values, changed by parse_cli()
+int delay = 5000;               // in ms
+size_t dtbufsize = 1024 * 1024; // in bytes
+
+void print_syntax(char *arg0) {
+  fprintf(stderr, "Syntax: %s [-b <dtbufsize>] <delay>\n", arg0);
+}
+
+// parse command: delay [-b <dtbufsize>] <delay_ms>
+// these commands should work:
+//   - delay 5s
+//   - delay -b 10m 4000
+//   - delay 4k -b 10000k
+void parse_cli(int argc, char *argv[]) {
+#define SYNTAX_ERROR 9
+#define OPT_NONE 0
+#define OPT_DTBUFSIZE 1
+#define ARG_VALUE 1
+#define ARG_DTBUFSIZE (1 << 1)
+  int ctx = OPT_NONE;
+  char *arg;
+  int args_handled = 0;
+  char *garbage;
+  int i;
+  for (i = 1; i < argc; i++) {
+    arg = argv[i];
+    if (ctx == OPT_DTBUFSIZE) {
+      if (args_handled & ARG_DTBUFSIZE) {
+        // dtbufsize parameter present twice
+        print_syntax(argv[0]);
+        exit(SYNTAX_ERROR);
+      }
+      // parse dtbufsize parameter
+      errno = 0;
+      dtbufsize = strtol(arg, &garbage, 0);
+      if (errno) {
+        perror("strtol()");
+        exit(10);
+      }
+      // handle modifiers for Kb, Mb and Gb
+      if (strcmp("k", garbage) == 0) {
+        dtbufsize <<= 10;
+      } else if (strcmp("m", garbage) == 0) {
+        dtbufsize <<= 20;
+      } else if (strcmp("g", garbage) == 0) {
+        dtbufsize <<= 30;
+      } else if (garbage[0] != '\0') {
+        fprintf(stderr, "dtbufsize value contains garbage: %s\n", garbage);
+        exit(SYNTAX_ERROR);
+      }
+      args_handled |= ARG_DTBUFSIZE;
+      ctx = OPT_NONE;
+    } else if (strcmp("-b", arg) == 0) {
+      ctx = OPT_DTBUFSIZE;
+    } else {
+      if (args_handled & ARG_VALUE) {
+        // delay value present twice
+        print_syntax(argv[0]);
+        exit(SYNTAX_ERROR);
+      }
+      // parse delay value
+      errno = 0;
+      delay = strtol(arg, &garbage, 0);
+      if (errno) {
+        perror("strtol()");
+        exit(10);
+      }
+      // handle modifiers for seconds, minutes and hours
+      if (strcmp("s", garbage) == 0) {
+        delay *= 1000;
+      } else if (strcmp("m", garbage) == 0) {
+        delay *= 60 * 1000;
+      } else if (strcmp("h", garbage) == 0) {
+        delay *= 60 * 1000 * 1000;      // likely to be useless
+      } else if (garbage[0] != '\0') {
+        fprintf(stderr, "delay value contains garbage: %s\n", garbage);
+        exit(SYNTAX_ERROR);
+      }
+      args_handled |= ARG_VALUE;
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   int r;                        // returned by poll()
@@ -32,7 +115,9 @@ int main(int argc, char *argv[]) {
   int in_closed = 0;
   int out_closed = 0;
 
-  if (dtbuf_init(&dtbuf, 1024 * 1024)) {
+  parse_cli(argc, argv);
+
+  if (dtbuf_init(&dtbuf, dtbufsize)) {
     fprintf(stderr, "dtbuf initialization failed\n");
     exit(1);
   }
@@ -47,7 +132,7 @@ int main(int argc, char *argv[]) {
     // we want to poll stdout if we have a next chunk to write now
     // if we only have a next chunk to write later, we set a timeout instead
     if (!out_closed && has_next_chunk) {
-      wait_delay = next_chunk_timestamp + DELAY - now;
+      wait_delay = next_chunk_timestamp + delay - now;
       if (wait_delay <= 0) {
         // data to write as soon as possible
         poll_stdout = 1;
