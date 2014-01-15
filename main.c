77c1fd2e8077521b353b734d4a7d20e21d58cdea
Romain Vimont (®om) <rom@rom1v.com>
2014-01-15 10:17:24 +0100

    Stop as soon as stdout is closed
    Even if stdin is alive or if we have chunks to write, we always want to
    detect and stop if stdout is closed.
    
    Thus, we always poll stdout, so it will interrupt any poll() call.

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include "dtbuf.h"
 #include "errno.h"
 #include "time_ms.h"
@@ -13,9 +12,10 @@
 
 struct dtbuf dtbuf;
 
+// stdout is at index 0 because we will always poll it
 struct pollfd fds_all[] = {
-  {0 /* stdin */ , POLLIN | POLLHUP},
-  {1 /* stdout */ , POLLOUT | POLLHUP}
+  {1 /* stdout */ , POLLOUT },
+  {0 /* stdin */ , POLLIN }
 };
 
 // default values, changed by parse_cli()
@@ -109,14 +109,18 @@ void parse_cli(int argc, char *argv[]) {
 
 int main(int argc, char *argv[]) {
   int r;                        // returned by poll()
-  struct pollfd *fds;           // selected fds
   int fds_count;
   int has_next_chunk = 0;
   time_ms next_chunk_timestamp;
   time_ms now;
   time_ms wait_delay;
+  // Note that stdin and stdout polling is not symetrical: we always poll
+  // stdout for detecting POLLERR or POLLHUP (in that case, we want to stop
+  // immediately), and we set POLLOUT only when we need to write.
+  // On the contrary, we poll stdin only when we need to read (we don't want
+  // to stop if we have chunks to write, even if stdin is closed).
   int poll_stdin;
-  int poll_stdout;
+  int pollout_stdout;
   int timeout;
   int in_closed = 0;
   int out_closed = 0;
@@ -135,56 +139,32 @@ int main(int argc, char *argv[]) {
     // we want to poll stdin when we can store what we will read
     poll_stdin = !in_closed && !dtbuf_is_full(&dtbuf);
 
-    // we want to poll stdout if we have a next chunk to write now
+    // here, out_closed is always false
+    // we want to pollout stdout if we have a next chunk to write now
     // if we only have a next chunk to write later, we set a timeout instead
-    if (!out_closed && has_next_chunk) {
+    if (has_next_chunk) {
       wait_delay = next_chunk_timestamp + delay - now;
       if (wait_delay <= 0) {
         // data to write as soon as possible
-        poll_stdout = 1;
+        pollout_stdout = 1;
         timeout = -1;
       } else {
         // data to write later
-        poll_stdout = 0;
+        pollout_stdout = 0;
         timeout = wait_delay;
       }
     } else {
       // no data to write at all
-      poll_stdout = 0;
+      pollout_stdout = 0;
       timeout = -1;
     }
 
-    if (!poll_stdin) {
-      if (!poll_stdout) {
-        // we do not want to poll at all, simply wait for timeout
-        if (timeout <= 0) {
-          // no poll, no timeout: nothing to do, we reached the end
-          goto end;
-        }
-        if (usleep(timeout * 1000)) {
-          perror("usleep()");
-          exit(2);
-        }
-        fds_count = 0;
-        // we won't call poll(), so we init the expected result
-        r = 0;
-      } else {
-        // we do not want to poll stdin, only stdout
-        fds = &fds_all[1];
-        fds_count = 1;
-      }
-    } else if (!poll_stdout) {
-      // we want to poll stdin only
-      fds = fds_all;
-      fds_count = 1;
-    } else {
-      // we want to poll both stdin and stdout
-      fds = fds_all;
-      fds_count = 2;
-    }
+    // we always want to poll stdout for detecting POLLERR or POLLHUP
+    fds_count = poll_stdin ? 2 : 1;
+    fds_all[0].events = pollout_stdout ? POLLOUT : 0;
 
-    // poll() if needed
-    if (fds_count > 0 && (r = poll(fds, fds_count, timeout)) == -1) {
+    // poll() the selected fds
+    if ((r = poll(fds_all, fds_count, timeout)) == -1) {
       perror("poll()");
       exit(3);
     }
@@ -196,17 +176,27 @@ int main(int argc, char *argv[]) {
         out_closed = 1;
       }
     } else {
-      if (poll_stdout && fds_all[1].revents) {
-        // read from dtbuf and write to stdout
-        if (dtbuf_read_chunk(&dtbuf, 1 /* stdout */ ) <= 0) {
+      if (fds_all[0].revents) {
+        // stdout has revents
+        if (fds_all[0].revents & (POLLOUT | POLLHUP)) {
+          // read from dtbuf and write to stdout
+          if (dtbuf_read_chunk(&dtbuf, 1 /* stdout */ ) <= 0) {
+            out_closed = 1;
+          }
+        } else {
           out_closed = 1;
         }
       }
-      if (poll_stdin && fds_all[0].revents) {
-        // we may have waited, get the new current time
-        now = get_time_ms();
-        // read from stdin and write to dtbuf
-        if (dtbuf_write_chunk(&dtbuf, 0 /* stdin */ , now) <= 0) {
+      if (poll_stdin && fds_all[1].revents) {
+        // stdin has revents
+        if (fds_all[1].revents & (POLLIN | POLLHUP)) {
+          // we may have waited, get the new current time
+          now = get_time_ms();
+          // read from stdin and write to dtbuf
+          if (dtbuf_write_chunk(&dtbuf, 0 /* stdin */ , now) <= 0) {
+            in_closed = 1;
+          }
+        } else {
           in_closed = 1;
         }
       }
@@ -217,9 +207,10 @@ int main(int argc, char *argv[]) {
     if (has_next_chunk) {
       dtbuf_peek_headers(&dtbuf, &next_chunk_timestamp, NULL);
     }
-  } while (!in_closed || (has_next_chunk && !out_closed));
+    // always stop on out_closed
+    // also stop when there will be no more chunks anymore
+  } while (!out_closed && (!in_closed || has_next_chunk));
 
-end:
   dtbuf_free(&dtbuf);
   return 0;
 }
